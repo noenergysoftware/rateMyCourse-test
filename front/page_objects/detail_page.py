073672e2f0141db541848a372eb1c555ebc61e2b
supplient <437566830@qq.com>
2019-05-10 17:27:22 +0800

    Remove uesless code in Detailpage.

--- front/page_objects/detail_page.py
+++ front/page_objects/detail_page.py
@@ -15,6 +15,14 @@ from .comment_page import CommentPage
 
 class DetailPage(SplitBasePage):
     def __init__(self, driver, url=None):
+
+        self.name_text_id = "course_name"
+        self.credit_text_id = "course_credit"
+        self.school_text_id = "course_school"
+        self.type_text_id = "course_type"
+        self.description_text_id = "coursedescription"
+
+        self.comment_page_btn_id = "toComment"
         self.comment_username_xpath = "./div[1]/p"
         self.comment_teachername_xpath = "./table[1]/tbody/tr/td[2]/p"
         self.comment_content_xpath = "./div[2]/p"
@@ -35,43 +43,17 @@ class DetailPage(SplitBasePage):
         super().__init__(
             driver,
             url,
-            prev_btn_loc=(By.XPATH, "//li[@id='lastpage']/a"),
-            next_btn_loc=(By.XPATH, "//li[@id='nextpage']/a"),
-            jump_text_loc=(By.ID, "jumpPage"),
-            jump_btn_loc=(By.XPATH, "//nobr[@id='jump']/button"),
-            now_index_text_loc=(By.ID, "pagenum"),
-            max_index_text_loc=(By.ID, "totalpage"),
-            block_div_loc=(By.ID, "comment"),
-            block_relative_loc=(By.XPATH, "./div"),
-            form_get_method=getForm,
+            prev_btn_loc        =(By.XPATH, "//li[@id='lastpage']/a"),
+            next_btn_loc        =(By.XPATH, "//li[@id='nextpage']/a"),
+            jump_text_loc       =(By.ID, "jumpPage"),
+            jump_btn_loc        =(By.XPATH, "//nobr[@id='jump']/button"),
+            now_index_text_loc  =(By.ID, "pagenum"),
+            max_index_text_loc  =(By.ID, "totalpage"),
+            block_div_loc       =(By.ID, "comment"),
+            block_relative_loc  =(By.XPATH, "./div"),
+            form_get_method     =getForm,
         )
 
-        self.comment_divs = None
-
-        self.name_text_id = "course_name"
-        self.credit_text_id = "course_credit"
-        self.school_text_id = "course_school"
-        self.type_text_id = "course_type"
-        self.description_text_id = "coursedescription"
-
-        # self.comment_big_div_id = 
-        # self.comment_sub_divs_xpath = 
-
-        self.comment_page_btn_id = "toComment"
-
-        # self.now_page_num_text_id = "pagenum"
-        # self.total_page_num_text_id = "totalpage"
-        # self.next_page_btn_xpath = 
-        # self.prev_page_btn_xpath = 
-        # self.jump_page_text_id = "jumpPage"
-        # self.jump_page_btn_xpath = 
-
-    # def splitedPageChange(f):
-        # @wraps(f)
-        # def wrap_func(self):
-            # self.comment_divs = None
-            # return f(self)
-        # return wrap_func
 
     def checkIsSelf(self):
         check_ids = [
@@ -85,53 +67,8 @@ class DetailPage(SplitBasePage):
         btn.click()
         return CommentPage(self.driver)
 
-    # def _getCommentDivs(self):
-        # if not self.comment_divs:
-            # comment_div = self.waitAppear_ID(self.comment_big_div_id)
-            # self.comment_divs = comment_div.find_elements_by_xpath(self.comment_sub_divs_xpath)
-        # return self.comment_divs
-
-    # def getCommentNum(self):
-        # return len(self._getCommentDivs())
-
-    # def getCommentForm(self, index):
-        # div = self._getCommentDivs()[index]
-        # res = {}
-        # temp = {
-            # "username": self.comment_username_xpath,
-            # "teachername": self.comment_teachername_xpath,
-            # "content": self.comment_content_xpath,
-            # "time": self.comment_time_xpath
-        # }
-        # for key, xpath in temp.items():
-            # res[key] = div.find_element_by_xpath(xpath).text
-        # return res
-
     def isCommentShow(self, index):
-        if index >= self.getCommentNum():
+        if index >= self.getBlockNum():
             raise Exception("Index {0} is too large.".format(index))
-        div = self._getCommentDivs()[index]
-        return not ("none" in div.get_attribute("style"))
-            
-    # @splitedPageChange
-    # def prevPage(self):
-        # btn = self.waitAppear_xpath(self.prev_page_btn_xpath)
-        # btn.click()
-
-    # @splitedPageChange
-    # def nextPage(self):
-        # btn = self.waitAppear_xpath(self.next_page_btn_xpath)
-        # btn.click()
-
-    # @splitedPageChange
-    # def jumpPage(self, index):
-        # text = self.waitAppear_ID(self.jump_page_text_id)
-        # text.send_keys(str(index))
-        # btn = self.waitAppear_xpath(self.jump_page_btn_xpath)
-        # btn.click()
-
-    # def getNowPageNum(self):
-        # return int(self.waitAppear_ID(self.now_page_num_text_id).text)
-
-    # def getTotalPageNum(self):
-        # return int(self.waitAppear_ID(self.total_page_num_text_id).text)
+        block = self.getBlocks()[index]
+        return not ("none" in block.get_attribute("style"))
